4ea2adedf8307a1d91a57c1b7b1e2d46ded80403
seth808rose <69048387+seth808rose@users.noreply.github.com>
2022-02-02 14:42:22 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data/data";
+import Questions from "./Questions";
+import {
+  AiFillInstagram,
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillGithub,
+} from "react-icons/ai";
 
 function App() {
+  // set up my useState
+  const [questions, setQuestions] = useState(data);
+
+  // console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='container'>
+        <h3>Questions and Answers</h3>
+        <section className='info'>
+          {questions.map((question) => {
+            return <Questions key={question.id} {...question} />;
+          })}
+        </section>
+        <footer>
+          <h4>Please visit my personal links</h4>
+          <div className='links'>
+            <a href='https://github.com/seth808rose'>
+              <AiFillGithub />
+            </a>
+            <a href='https://twitter.com/rose_seta'>
+              <AiFillTwitterCircle />
+            </a>
+            <a href='https://www.linkedin.com/in/seta-rose-33014219a/'>
+              <AiFillLinkedin />
+            </a>
+          </div>
+        </footer>
+      </div>
+    </main>
   );
 }
 
